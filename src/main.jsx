62e77d2181ecf81d53076c1c411f58c85c530107
LexMurUa <lex16mur@gmail.com>
2025-09-01 23:52:43 +0300

    add adaptive

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.scss";
-import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.scss";
 import { WeatherInfo } from "./Pages/WeatherInfo/WeatherInfo.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter
+      basename="WeatherReactViteApi"
       future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
     >
       <Routes>
